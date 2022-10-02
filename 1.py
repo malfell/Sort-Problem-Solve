37574adfaed3e6d73d8ad8c9e009aeff8f85345a
Mal F <malfellows13@gmail.com>
2022-10-02 10:26:40 -0500

    problem 1 and 2 done

--- 1.py
+++ 1.py
@@ -1,1 +1,13 @@
 # Write your solution for algorithm 1 below
+
+'''
+sort() method: modifies and sorts a list
+sorted() function: creates a new sorted list
+'''
+
+# Write an algorithm that takes in an unsorted numerical list 
+# as an argument and sorts the list (use the sort() method).
+
+lst = [99, 4, 3, 5]
+print(lst.sort())
+print(lst)
--- 2.py
+++ 2.py
@@ -1,1 +1,10 @@
 # Write your solution for algorithm 2 below
+
+'''
+Write an algorithm that takes in an unsorted numerical list as an argument 
+and creates a new sorted list (use the sorted() function).
+'''
+
+lst2 = [7, 0, 4, 11, 10]
+new_lst = sorted(lst2)
+print(new_lst)
